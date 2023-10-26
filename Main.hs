d4ac178404710c605b41a4671fa0e5d4136eee82
Javier Pérez Arteaga <surtich@gmail.com>
2023-10-26 02:38:08 +0200

    The Book of Monads - ch4 - Utilities for Monadic Code

--- Main.hs
+++ Main.hs
@@ -1,92 +1,176 @@
-{-# LANGUAGE MonadComprehensions #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Eta reduce" #-}
-{-# HLINT ignore "Redundant lambda" #-}
-{-# HLINT ignore "Use evalState" #-}
-{-# HLINT ignore "Use const" #-}
+module Chapter4 where
+import Data.Traversable (for)
+import Text.XHtml (action, ins)
+import qualified Control.Monad as Chapter4
 
-module Chapter3 where 
+doesNotWork :: [IO ()]
+doesNotWork = map (\name -> print ("Hello, " ++ name)) ["Alejandro", "Elena"]
 
-import Data.Char (toUpper)
+mapM' :: (Monad m) => (a -> m b) -> [a] -> m [b]
+mapM' f as = sequence' (map f as)
 
-plus :: Maybe Int -> Maybe Int -> Maybe Int
-plus x y = x >>= \x' -> y >>= \y' -> return (x' + y')
+mapM'' :: (Monad m) => (a -> m b) -> [a] -> m [b]
+mapM'' f [] = return []
+mapM'' f (a:as) = f a >>= \b -> mapM'' f as >>= \bs -> return (b:bs)
 
-lift2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
-lift2 f = \ma mb -> ma >>= \a -> mb >>= \b -> return (f a b)
+works :: IO[()]
+works = mapM' (\name -> print ("Hello, " ++ name)) ["Alejandro", "Elena"]
 
-maybeEight = lift2 (+)  (Just 3) (Just 5)
+forM :: (Monad m) => [a] -> (a -> m b) -> m [b]
+forM = flip mapM'
 
-ap :: Monad m => m (b -> c) -> m b -> m c
-ap mf mb = mb >>= \b -> mf >>= \f -> return (f b)
+-- similar to a for loop in other languages
+works' = forM ["Alejandro", "Elena"] (\name -> print ("Hello, " ++ name))
 
-lift2' :: Monad m => (a -> b -> c) -> m a -> m b -> m c
-lift2' f ma mb = fmap f ma `ap` mb
+void :: Functor m => m a -> m ()
+void = fmap (const ())
 
-lift2'' :: Monad m => (a -> b -> c) -> m a -> m b -> m c
-lift2'' f = ap.fmap f
+-- avoid output of the list of IO actions
+works'' :: IO ()
+works'' = void $ forM ["Alejandro", "Elena"] (\name -> print ("Hello, " ++ name))
 
-fmap' :: (Monad f) =>  (a -> b) -> f a  -> f b
-fmap' f = ap (pure f)
+forM_ :: (Monad m) => [a] -> (a -> m b) -> m ()
+forM_ xs f = void $ forM xs f 
 
-newtype ZipList a = ZipList { getZipList :: [a]}
+sequence' :: (Monad m) => [m a] -> m [a]
+sequence' [] = pure []
+sequence' (ma:ms) = ma >>= \a -> sequence' ms >>= \as -> return (a:as)
 
-instance (Show a) => Show (ZipList a) where
-  show (ZipList xs) = show xs
+sequence'' :: (Monad m) => [m a] -> m [a]
+sequence'' = mapM' id
 
-instance Functor ZipList where
-  fmap f (ZipList xs) = ZipList (fmap f xs)
+-- In module Control.Monad, package base
+zipWithM :: Monad m => (a -> b -> m c) -> [a] -> [b] -> m [c]
+zipWithM f xs ys = sequence' (zipWith f xs ys)
 
-instance Applicative ZipList where
-  pure x = ZipList [x]
-  ZipList fs <*> ZipList xs = ZipList (zipWith id fs xs)
+replicateM :: Monad m => Int -> m a -> m [a]
+replicateM f = sequence' . replicate f
 
-(<$) :: Functor f => a -> f b -> f a
-(<$) = fmap . const
+filterM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+filterM _ [] = return []
+filterM p (x:xs) = do
+                    b      <- p x
+                    xs'    <- filterM p xs
+                    return (if b then x:xs' else xs')
 
-(<*) :: Applicative f => f a -> f b -> f a
-(<*) fa fb = const <$> fa <*> fb
+-- foldM performs a left fold, like foldl
+foldM :: Monad m => (b -> a -> m b) -> b -> [a] -> m b
+foldM f a [] = return a
+foldM f a (x:xs) = f a x >>= \y -> foldM f y xs
 
-(*>) :: Applicative f => f a -> f b -> f b
-(*>) fa fb = (\ _ x -> x) <$> fa <*> fb
+-- In module Control.Monad.Extra, package extra
+partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a], [a])
+partitionM p = foldM f ([], [])
+    where f (a, b) x = p x >>= \y -> return (if y then (x:a, b) else (a, x:b))
 
-type Name = String
-data Person = Person { name :: Name, age :: Int }
+concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
+concatMapM f = foldM (\a b -> f b >>= \c -> return (a ++ c)) []
 
-validateName :: Name -> Maybe Name
-validateName = undefined
+-- In module Control.Monad.Loops, package monad-loops
+-- and Control.Monad.Extra, package extra
+andM :: Monad m => [m Bool] -> m Bool
+andM = allM id
 
-validateAge :: Int -> Maybe Int
-validateAge = undefined
+orM :: Monad m => [m Bool] -> m Bool
+orM = anyM id
 
-validatePerson :: Name -> Int -> Maybe Person
-validatePerson name age = validateName name >>= \name' ->
-                          validateAge  age  >>= \age'  ->
-                          return (Person name' age')
+anyM :: Monad m => (a -> m Bool) -> [a] -> m Bool
+anyM p = orM . map p
 
-upperName :: Name -> Int -> Maybe [Char]
-upperName name age = map toUpper <$> validateName name Prelude.<* validateAge age
+allM :: Monad m => (a -> m Bool) -> [a] -> m Bool
+allM p = andM . map p
 
-upperName' name age = map toUpper Prelude.<$ validateAge age <*> validateName name
+-- In module Control.Monad.Loops, package monad-loops
+takeWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+takeWhileM _ [] = return []
+takeWhileM p (x:xs) = p x >>= \y -> if y then takeWhileM p xs >>= \ys -> return (x:ys) else return []
 
-person1 :: Name -> Maybe Person
-person1 name = Person <$> validateName name <*> pure 20
+dropWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+dropWhileM _ [] = return []
+dropWhileM p (x:xs) = p x >>= \y -> if y then dropWhileM p xs else return (x:xs)
 
-toNestedTriple :: (a, b, c) -> (a, (b, c))
-toNestedTriple (a, b, c) = (a, (b, c))
+firstM :: Monad m => (a -> m Bool) -> [a] -> m (Maybe a)
+firstM _ [] = return Nothing
+firstM p (x:xs) = p x >>= \y -> if y then return (Just x) else firstM p xs
 
-toNestedQuadruple :: (a, b, c, d) -> (a, (b, (c, d)))
-toNestedQuadruple (a, b, c, d) = (a, toNestedTriple (b, c, d))
+when :: Monad m => Bool -> m () -> m ()
+when True  action = action
+when False _      = return ()
 
-class Applicative f => Monoidal f where
-  unit :: f ()
-  unit = pure ()
+unless :: Monad m => Bool -> m () -> m ()
+unless cond = when (not cond)
 
-  (**) :: f a -> f b -> f (a, b)
-  (**) fa fb = (,) <$> fa <*> fb
+ifM :: Monad m => m Bool -> m a -> m a -> m a
+ifM cond th el = do c <-  cond
+                    if c then th else el
+-- Alternative definition
+-- ifM = liftM3 (\c t e -> if c then t else e)
 
-pure' :: (Monoidal f) => a -> f a
-pure' a = fmap (\_ -> a) unit
+-- how can it return a value of any type b you want? The trick
+-- is that forever f never returns, so it is free to promise whatever it wants — it will
+-- never fulfill that promise anyway.
+forever :: Monad m => m a -> m b
+forever action = do action
+                    forever action
 
-ap' :: (Monoidal f) => f (a -> b) -> f a -> f b
-ap' ff fa = fmap (\(g, a) -> g a) (ff Chapter3.** fa)
+whileM :: Monad m => m Bool -> m a -> m [a]
+whileM cond action = do c <- cond
+                        if c
+                        then (:) <$> action <*> whileM cond action
+                        else return []
+
+whileM_ :: Monad m => m Bool -> m a -> m ()
+whileM_ cond action = void $ whileM cond action
+
+-- The function iterateWhile also checks a condition until it fails, but it does
+-- so on the result of the previous iteration. For that reason, you do not get a list of
+-- values, as with whileM , just the one that makes the iteration stop
+iterateWhile :: Monad m => (a -> Bool) -> m a -> m a
+iterateWhile  cond action = do a <- action
+                               if cond a 
+                               then iterateWhile cond action
+                               else return a
+
+iterateUntilM :: Monad m => (a -> m Bool) -> (a -> m a) -> a -> m a
+iterateUntilM cond action a  = do c <- cond a
+                                  if c
+                                  then return a
+                                  else action a >>= iterateUntilM cond action
+
+loopM :: Monad m => (a -> m (Either a b)) -> a -> m b
+loopM cond x = do r <- cond x
+                  case r of
+                    Left next -> loopM cond next
+                    Right end -> return end
+
+class Functor f => Traversable f where
+  traverse :: Applicative m => (a -> m b) -> f a -> m (f b)
+
+  mapM :: Monad m => (a -> m b) -> f a -> m (f b)
+  mapM = Chapter4.traverse
+
+  sequenceA :: Applicative m => f (m a) -> m (f a)
+  sequenceA = Chapter4.traverse id
+
+  sequence :: Monad m => f (m a) -> m (f a)
+  sequence = Chapter4.sequenceA
+
+instance Chapter4.Traversable Maybe where
+  traverse  _ Nothing = pure Nothing
+  traverse  f (Just x) = Just <$> f x
+
+allJust :: [Maybe a] -> Maybe [a]
+allJust = Prelude.sequenceA
+
+newtype Identity a = Identity a -- do nothing with the value
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure = Identity
+  Identity f <*> Identity a = Identity (f a)
+
+instance Monad Identity where
+  return = pure
+  Identity a >>= f = f a
